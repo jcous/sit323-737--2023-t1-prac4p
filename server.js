bc753a8e2329c14118c4ea24dd63cad2a36453d1
jcous <46913336+jcous@users.noreply.github.com>
2023-04-09 11:57:06 +1000

    Update server.js
    Added more specific error handling + a log file to record errors.

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "calcMicroFile.log" }),
+  ],
+});
 
 app.use(express.json());
 //addition endpoint
@@ -7,9 +17,15 @@ app.get("/add", (req, res) => {
   const num1 = parseFloat(req.query.num1);
   const num2 = parseFloat(req.query.num2);
 
-  // Check if num1 and num2 are valid numbers
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).json({ error: "Invalid input parameters" });
+  if (isNaN(num1) && isNaN(num2)) {
+    res.status(400).json({ error: "Both input parameters are invalid" });
+    logger.error("Both input parameters are invalid");
+  } else if (isNaN(num1)) {
+    res.status(400).json({ error: "num1 is invalid" });
+    logger.error("num1 is invalid");
+  } else if (isNaN(num2)) {
+    res.status(400).json({ error: "num2 is invalid" });
+    logger.error("num2 is invalid");
   } else {
     const result = num1 + num2;
     res.json({ result: result });
@@ -20,8 +36,15 @@ app.get("/subtract", (req, res) => {
   const num1 = parseFloat(req.query.num1);
   const num2 = parseFloat(req.query.num2);
 
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).json({ error: "Invalid input parameters" });
+  if (isNaN(num1) && isNaN(num2)) {
+    res.status(400).json({ error: "Both input parameters are invalid" });
+    logger.error("Both input parameters are invalid");
+  } else if (isNaN(num1)) {
+    res.status(400).json({ error: "num1 is invalid" });
+    logger.error("num1 is invalid");
+  } else if (isNaN(num2)) {
+    res.status(400).json({ error: "num2 is invalid" });
+    logger.error("num2 is invalid");
   } else {
     const result = num1 - num2;
     res.json({ result: result });
@@ -32,8 +55,15 @@ app.get("/multiply", (req, res) => {
   const num1 = parseFloat(req.query.num1);
   const num2 = parseFloat(req.query.num2);
 
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).json({ error: "Invalid input parameters" });
+  if (isNaN(num1) && isNaN(num2)) {
+    res.status(400).json({ error: "Both input parameters are invalid" });
+    logger.error("Both input parameters are invalid");
+  } else if (isNaN(num1)) {
+    res.status(400).json({ error: "num1 is invalid" });
+    logger.error("num1 is invalid");
+  } else if (isNaN(num2)) {
+    res.status(400).json({ error: "num2 is invalid" });
+    logger.error("num2 is invalid");
   } else {
     const result = num1 * num2;
     res.json({ result: result });
@@ -44,11 +74,20 @@ app.get("/division", (req, res) => {
   const num1 = parseFloat(req.query.num1);
   const num2 = parseFloat(req.query.num2);
 
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).json({ error: "Invalid input parameters" });
+  if (isNaN(num1) && isNaN(num2)) {
+    res.status(400).json({ error: "Both input parameters are invalid" });
+    logger.error("Both input parameters are invalid");
+  } else if (isNaN(num1)) {
+    res.status(400).json({ error: "num1 is invalid" });
+    logger.error("num1 is invalid");
+  } else if (isNaN(num2)) {
+    res.status(400).json({ error: "num2 is invalid" });
+    logger.error("num2 is invalid");
+
     //check if num2 equal 0
   } else if (num2 === 0) {
     res.status(400).json({ error: "Cannot divide by zero" });
+    logger.error("Cannot divide by zero");
   } else {
     const result = num1 / num2;
     res.json({ result: result });
@@ -56,5 +95,5 @@ app.get("/division", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Calculator microservice listening on port 3000");
+  logger.info("Calculator microservice listening on port 3000");
 });
